aa7d23338f99545162e85ff54ebd2aa0ff185ffc
PSivkova <165670995+PSivkova@users.noreply.github.com>
2024-05-22 21:27:53 +0700

    Add files via upload

---
+++ homework15.py
@@ -0,0 +1,13 @@
+#Создайте новый класс House
+#Задайте ему новый атрибут numberOfFloors = 10
+#В цикле пройдитесь по атрибуту numberOfFloors и распечайте значение "Текущий этаж равен"
+
+class House:
+    umberOfFloors = 10
+
+
+floor = House()
+a = 0
+while a < floor.umberOfFloors:
+    a += 1
+    print("Текущий этаж равен ", a)
